b0059686739c4c8806aa6c38e7056098cb31a99d
Akshay Mittal <mittalakshay6@gmail.com>
2025-01-26 23:33:44 +0530

    remove Workflow Node Definition

--- src/main/java/workflow/WorkflowNodeDefinition.java
+++
@@ -1,7 +0,0 @@
-package workflow;
-
-import workflow.exceptions.errorhandlers.IWorkflowErrorHandler;
-
-public record WorkflowNodeDefinition(String id, IWorkflowNode nodeInstance, Class<? extends IWorkflowErrorHandler> errorHandler) {
-
-}
