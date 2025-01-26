967491c4b8211280388bb3b3885b845efbacb633
Akshay Mittal <mittalakshay6@gmail.com>
2025-01-26 23:33:09 +0530

    implement WorkflowException

--- src/main/java/workflow/exceptions/WorkflowException.java
+++ src/main/java/workflow/exceptions/WorkflowException.java
@@ -1,11 +1,7 @@
 package workflow.exceptions;
 
-public class WorkflowException extends RuntimeException {
+public class WorkflowException extends RuntimeException implements IWorkflowException {
     public WorkflowException(String message) {
         super(message);
-    }
-
-    public WorkflowException(String message, Throwable cause) {
-        super(message, cause);
     }
 }
