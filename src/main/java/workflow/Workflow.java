c19573272a4704d0087a9178f5e5997043915b21
Akshay Mittal <mittalakshay6@gmail.com>
2025-01-28 22:01:06 +0530

    add workflow id

--- src/main/java/workflow/Workflow.java
+++ src/main/java/workflow/Workflow.java
@@ -5,10 +5,32 @@ import lombok.NonNull;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public record Workflow(Map<Class<? extends IWorkflowTask>, Set<Class<? extends IWorkflowTask>>> adjacency) {
+public record Workflow(String id, Map<Class<? extends IWorkflowTask>, Set<Class<? extends IWorkflowTask>>> adjacency) {
 
-    public Workflow(@NonNull Map<Class<? extends IWorkflowTask>, Set<Class<? extends IWorkflowTask>>> adjacency) {
+    public Workflow(@NonNull String id,
+            @NonNull Map<Class<? extends IWorkflowTask>, Set<Class<? extends IWorkflowTask>>> adjacency) {
+        this.id = id;
         this.adjacency = Collections.unmodifiableMap(adjacency);
+    }
+
+    @Override
+    public String toString() {
+        return "Workflow{" + "id='" + id + '\'' + ", adjacency=" + getAdjacencyString() + '}';
+    }
+
+    private String getAdjacencyString() {
+        StringBuilder builder = new StringBuilder();
+        for (Map.Entry<Class<? extends IWorkflowTask>, Set<Class<? extends IWorkflowTask>>> entry : this.adjacency()
+                .entrySet()) {
+            Class<? extends IWorkflowTask> task = entry.getKey();
+            Set<Class<? extends IWorkflowTask>> nextTasks = entry.getValue();
+
+            builder.append(task.getSimpleName()).append(" -> [");
+            builder.append(nextTasks.stream().map(Class::getSimpleName).collect(Collectors.joining(", ")));
+            builder.append("]\n");
+        }
+        return builder.toString();
     }
 }
