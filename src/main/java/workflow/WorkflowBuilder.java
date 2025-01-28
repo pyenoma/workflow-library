d21a8de49b31b392f7a7564ca40c4244ac14cdc7
Akshay Mittal <mittalakshay6@gmail.com>
2025-01-28 22:01:58 +0530

    Modify workflow builder to execute validations when workflow is built

--- src/main/java/workflow/WorkflowBuilder.java
+++ src/main/java/workflow/WorkflowBuilder.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import workflow.annotations.WorkflowDefinition;
 import workflow.annotations.WorkflowTask;
 import workflow.validators.services.WorkflowDefinitionValidationService;
+import workflow.validators.services.WorkflowValidationService;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -24,6 +25,8 @@ public class WorkflowBuilder {
     private final ApplicationContext applicationContext;
 
     private final WorkflowDefinitionValidationService workflowDefinitionValidationService;
+
+    private final WorkflowValidationService workflowValidationService;
 
     private final WorkflowRegistry workflowRegistry;
 
@@ -44,17 +47,18 @@ public class WorkflowBuilder {
         workflowDefinitionBeanTypes.forEach(workflowDefinitionBeanType -> {
             String workflowId = getWorkflowId(workflowDefinitionBeanType);
             workflowDefinitionValidationService.validate(workflowId, workflowDefinitionBeanType);
-            workflowRegistry.register(workflowId,
-                    build(Arrays.stream(workflowDefinitionBeanType.getAnnotation(WorkflowDefinition.class).tasks())
-                            .toList()));
+            Workflow workflow = build(workflowId,
+                    Arrays.stream(workflowDefinitionBeanType.getAnnotation(WorkflowDefinition.class).tasks()).toList());
+            workflowValidationService.validate(workflow);
+            workflowRegistry.register(workflowId, workflow);
         });
     }
 
-    private Workflow build(List<WorkflowTask> workflowTasks) {
+    private Workflow build(String workflowId, List<WorkflowTask> workflowTasks) {
         Map<Class<? extends IWorkflowTask>, Set<Class<? extends IWorkflowTask>>> adjacency = new HashMap<>();
         workflowTasks.forEach(workflowTask -> adjacency.put(workflowTask.taskClass(),
                 Arrays.stream(workflowTask.next()).collect(Collectors.toSet())));
-        return new Workflow(adjacency);
+        return new Workflow(workflowId, adjacency);
     }
 }
 
